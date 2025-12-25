41aa4e1d61e89c93d3051c58610be80edbdc27d0
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-25 13:47:04 -0500

    redo UISound header

---
+++ src/UISound.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+class UISound : public sf::Drawable
+{
+public:
+  UISound();
+
+  void setScore(int value);
+  void initiate(bool state);
+  void gameOver();
+  void wing();
+
+private:
+  void draw(sf::RenderTarget &target,
+            sf::RenderStates states) const override;
+
+  void centerText(sf::Text &text);
+  void loadMaxScore();
+  void saveMaxScore();
+
+private:
+  // ===== Audio =====
+  sf::SoundBuffer pointBuffer;
+  sf::SoundBuffer wingBuffer;
+  sf::SoundBuffer hitBuffer;
+
+  sf::Sound pointSound;
+  sf::Sound wingSound;
+  sf::Sound hitSound;
+
+  sf::Music music;
+
+  // ===== UI =====
+  sf::Font font;
+  sf::Text scoreText;
+  sf::Text maxScoreText;
+
+  sf::Texture gameOverTexture;
+  sf::Sprite gameOverSprite;
+
+  sf::Texture initTexture;
+  sf::Sprite initSprite;
+
+  // ===== State =====
+  int score = 0;
+  int maxScore = 0;
+  bool initiated = false;
+  bool isGameOver = false;
+};
