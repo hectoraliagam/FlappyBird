749f3f2072f9d5f617353bfe1ba087e22c7dfad1
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-25 13:28:56 -0500

    redo Parallax header

---
+++ src/Parallax.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "Obstacle.hpp"
+
+class Parallax : public sf::Drawable
+{
+public:
+  Parallax();
+
+  void update();
+  bool checkCollision(const sf::IntRect &rect) const;
+  void initiate();
+
+  int getScore() const;
+
+private:
+  void updateBases();
+  void updateObstacles();
+  int randomPipeHeight() const;
+
+  void draw(sf::RenderTarget &target,
+            sf::RenderStates states) const override;
+
+private:
+  bool initiated = false;
+  int score = 0;
+  int lastPassedIndex = -1;
+
+  sf::Texture obstacleTexture;
+  sf::Texture baseTexture;
+
+  std::vector<sf::Sprite> bases;
+  std::vector<Obstacle> obstacles;
+};
